8588479e0d9b6eab2640bd9896914b2f273f1c19
Adam Paynter <adampaynter@gmail.com>
2006-06-15 02:17:43 +0000

    added link

---
+++ src/net/lucenews/opensearch/OpenSearchLink.java
@@ -0,0 +1,17 @@
+public class OpenSearchLink {
+    
+    private String href;
+    private String hreflang;
+    
+    
+    
+    
+    public String getHref () {
+        return href;
+    }
+    
+    public void setHref (String href) {
+        this.href = href;
+    }
+    
+}
--- src/net/lucenews/opensearch/OpenSearchResponse.java
+++ src/net/lucenews/opensearch/OpenSearchResponse.java
@@ -3,5 +3,6 @@ public class OpenSearchResponse {
     private Integer total_results;
     private Integer start_index;
     private Integer items_per_page;
+    private OpenSearchLink link;
     
 }
