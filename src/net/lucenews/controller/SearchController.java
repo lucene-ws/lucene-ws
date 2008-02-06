e7745a053fc791babad2e292fb613c202d40d3a8
Brian Cassidy <bricas@cpan.org>
2008-02-06 20:50:37 +0000

    revert 484

--- src/net/lucenews/controller/SearchController.java
+++ src/net/lucenews/controller/SearchController.java
@@ -72,7 +72,7 @@ public class SearchController extends Controller {
          * Prepare the sort by isolating invalid sort fields
          */
 
-        boolean isValidSortField = false;
+        boolean isSortable = false;
         
         Sort sort = c.getSort();
         if (sort != null) {
@@ -91,7 +91,7 @@ public class SearchController extends Controller {
 	        	SortField sortField = i.next();
 	        	
 	        	if (sortField.getField() != null) {
-		        	isValidSortField = true;
+		        	boolean isValidSortField = true;
 		        	
 		        	for (int j = 0; j < indices.length; j++) {
 		        		TermEnum terms = readers[ j ].terms(new Term(sortField.getField(), ""));
@@ -183,7 +183,7 @@ public class SearchController extends Controller {
          */
         
         Hits hits= null;
-        if (isValidSortField) {
+        if (isSortable) {
             hits = searcher.search( query, c.getFilter(), c.getSort() );    
         }
         else {
