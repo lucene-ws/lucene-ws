2cebc55aab58f476103db2838e03f1db7acef9be
Adam Paynter <adampaynter@gmail.com>
2008-02-12 16:56:08 +0000

    tidying up

--- src/net/lucenews3/opensearch/QueryImpl.java
+++ src/net/lucenews3/opensearch/QueryImpl.java
@@ -1,8 +1,5 @@
 package net.lucenews3.opensearch;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * OpenSearch Query provides a way of referring to a specific set of search
  * results. It is simple, powerful and flexible, and has a wide variety of uses.
@@ -15,192 +12,105 @@ import java.util.Map;
 public class QueryImpl implements Query {
 
 	private String role;
-
 	private String title;
-
 	private String osd;
-
 	private Integer total_results;
-
 	private String search_terms;
-
 	private Integer count;
-
 	private Integer start_index;
-
 	private Integer start_page;
-
 	private String language;
-
 	private String output_encoding;
-
 	private String input_encoding;
 
-	private Map<String, String> namespaces;
-
-	public QueryImpl() {
-		namespaces = new LinkedHashMap<String, String>();
-	}
-
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getRole()
-	 */
 	public String getRole() {
 		return role;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setRole(java.lang.String)
-	 */
 	public void setRole(String role) {
 		this.role = role;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getTitle()
-	 */
 	public String getTitle() {
 		return title;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setTitle(java.lang.String)
-	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getOsd()
-	 */
 	public String getOsd() {
 		return osd;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setOsd(java.lang.String)
-	 */
 	public void setOsd(String osd) {
 		this.osd = osd;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getTotalResults()
-	 */
 	public Integer getTotalResults() {
 		return total_results;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setTotalResults(java.lang.Integer)
-	 */
 	public void setTotalResults(Integer total_results) {
 		this.total_results = total_results;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getSearchTerms()
-	 */
 	public String getSearchTerms() {
 		return search_terms;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setSearchTerms(java.lang.String)
-	 */
 	public void setSearchTerms(String search_terms) {
 		this.search_terms = search_terms;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getCount()
-	 */
 	public Integer getCount() {
 		return count;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setCount(java.lang.Integer)
-	 */
 	public void setCount(Integer count) {
 		this.count = count;
 	}
-
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getStartIndex()
-	 */
 
 	public Integer getStartIndex() {
 		return start_index;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setStartIndex(java.lang.Integer)
-	 */
 	public void setStartIndex(Integer start_index) {
 		this.start_index = start_index;
 	}
-
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getStartPage()
-	 */
 
 	public Integer getStartPage() {
 		return start_page;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setStartPage(java.lang.Integer)
-	 */
 	public void setStartPage(Integer start_page) {
 		this.start_page = start_page;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getLanguage()
-	 */
 	public String getLanguage() {
 		return language;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setLanguage(java.lang.String)
-	 */
 	public void setLanguage(String language) {
 		this.language = language;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getOutputEncoding()
-	 */
 	public String getOutputEncoding() {
 		return output_encoding;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setOutputEncoding(java.lang.String)
-	 */
 	public void setOutputEncoding(String output_encoding) {
 		this.output_encoding = output_encoding;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getInputEncoding()
-	 */
 	public String getInputEncoding() {
 		return input_encoding;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#setInputEncoding(java.lang.String)
-	 */
 	public void setInputEncoding(String input_encoding) {
 		this.input_encoding = input_encoding;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getBoundingIndices()
-	 */
 	public Integer[] getBoundingIndices() {
 		Integer[] bounds = new Integer[] { null, null };
 
@@ -235,25 +145,14 @@ public class QueryImpl implements Query {
 		return bounds;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getFirstIndex()
-	 */
-
 	public Integer getFirstIndex() {
 		return getBoundingIndices()[0];
 	}
-
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getLastIndex()
-	 */
 
 	public Integer getLastIndex() {
 		return getBoundingIndices()[1];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getPagingBoundaries()
-	 */
 	public Integer[] getPagingBoundaries() {
 		Integer[] boundaries = new Integer[2];
 
@@ -286,16 +185,10 @@ public class QueryImpl implements Query {
 		return boundaries;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getFirstPage()
-	 */
 	public Integer getFirstPage() {
 		return getPagingBoundaries()[0];
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getPreviousPage()
-	 */
 	public Integer getPreviousPage() {
 		Integer startPage = getStartPage();
 		Integer firstPage = getFirstPage();
@@ -311,9 +204,6 @@ public class QueryImpl implements Query {
 		return startPage - 1;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getNextPage()
-	 */
 	public Integer getNextPage() {
 		Integer startPage = getStartPage();
 		Integer lastPage = getLastPage();
@@ -329,9 +219,6 @@ public class QueryImpl implements Query {
 		return startPage + 1;
 	}
 
-	/* (non-Javadoc)
-	 * @see net.lucenews3.opensearch.Query#getLastPage()
-	 */
 	public Integer getLastPage() {
 		return getPagingBoundaries()[1];
 	}
