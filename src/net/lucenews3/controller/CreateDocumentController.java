37553dac063d48240d03514c146fbdf175ce17f6
Adam Paynter <adampaynter@gmail.com>
2008-02-19 11:38:27 +0000

    few little changes

--- src/net/lucenews3/controller/CreateDocumentController.java
+++ src/net/lucenews3/controller/CreateDocumentController.java
@@ -1,60 +1,34 @@
 package net.lucenews3.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.lucenews3.atom.Entry;
-import net.lucenews3.lucene.support.Document;
 import net.lucenews3.lucene.support.DocumentList;
 import net.lucenews3.lucene.support.Index;
-import net.lucenews3.lucene.support.NativeDocumentDocument;
+import net.lucenews3.lucene.support.IndexIdentity;
+import net.lucenews3.lucene.support.IndexIdentityParser;
 
-import org.apache.lucene.document.Field;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 public class CreateDocumentController implements Controller {
 
-	private IndexAccess indexAccess;
-	private EntryResolver entryResolver;
+	private IndexIdentityParser<HttpServletRequest> indexIdentityParser;
+	private Map<IndexIdentity, Index> indexesByIdentity;
 	
 	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		Index index = indexAccess.get(request.getAttribute("index").toString());
-		DocumentList documents = index.getDocuments();
+		// Resolve index
+		final IndexIdentity indexIdentity = indexIdentityParser.parse(request);
+		final Index index = indexesByIdentity.get(indexIdentity);
 		
-		List<Entry> entries = entryResolver.resolveEntries(request);
+		// Resolve documents
+		final DocumentList documents = null;
+		index.getDocuments().addAll(documents);
 		
-		for (Entry entry : entries) {
-			Document document = buildDocument(entry);
-			documents.add(document);
-		}
-		
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * Builds a document from an Atom entry.
-	 * @param entry
-	 * @return
-	 */
-	public Document buildDocument(Entry entry) {
-		Document result = new NativeDocumentDocument();
-		buildFields(entry, result.getFields());
-		return result;
-	}
-	
-	public List<Field> buildFields(Entry entry) {
-		List<Field> results = new ArrayList<Field>();
-		return results;
-	}
-	
-	public void buildFields(Entry entry, List<Field> fields) {
-		
+		return new ModelAndView("document/create", "documents", documents);
 	}
 
 }
--- src/net/lucenews3/controller/RemoveDocumentController.java
+++ src/net/lucenews3/controller/RemoveDocumentController.java
@@ -1,19 +1,30 @@
 package net.lucenews3.controller;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.lucenews3.lucene.support.Index;
+import net.lucenews3.lucene.support.IndexIdentity;
+import net.lucenews3.lucene.support.IndexIdentityParser;
 
 import org.springframework.web.servlet.ModelAndView;
 
 public class RemoveDocumentController extends AbstractController {
 
+	private IndexIdentityParser<HttpServletRequest> indexIdentityParser;
+	private Map<IndexIdentity, Index> indexesByIdentity;
+	
 	public ModelAndView handleRequest(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
-		Index index = service.getIndex(request.getAttribute("index").toString());
-		// TODO Auto-generated method stub
-		return null;
+		// Resolve index
+		final IndexIdentity indexIdentity = indexIdentityParser.parse(request);
+		final Index index = indexesByIdentity.get(indexIdentity);
+		
+		
+		
+		return new ModelAndView("document/remove", "index", index);
 	}
 
 }
