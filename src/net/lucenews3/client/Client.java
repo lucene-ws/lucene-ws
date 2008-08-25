8bfe9baecf3bae18b815a215f306bbc3179d276b
Adam Paynter <adampaynter@gmail.com>
2008-08-25 09:41:40 +0000

    more libraries

--- src/net/lucenews3/client/Client.java
+++ src/net/lucenews3/client/Client.java
@@ -1,27 +1,50 @@
 package net.lucenews3.client;
 
-import static javax.xml.stream.XMLStreamReader.*;
+import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
 
-import java.util.Map.Entry;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Map.Entry;
 
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
 
 import net.lucenews3.XMLStreamUtility;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
 
 public class Client {
 
 	private String url;
+	private XMLOutputFactory xmlOutputFactory;
+	private HttpClient httpClient;
+
+	public static void main(String... args) throws Exception {
+		Client client = new Client("http://localhost:8080/lucene");
+		Document document = new Document();
+		document.add(new Field("name", "stan", Field.Store.YES, Field.Index.TOKENIZED));
+		client.insertDocument("test", document);
+	}
 
 	public Client(String url) {
 		this.url = url;
+		this.xmlOutputFactory = XMLOutputFactory.newInstance();
+		this.httpClient = new HttpClient();
+	}
+
+	public String getIndexURI(String index) {
+		return "http://localhost:8080/lucene/" + index;
 	}
 
 	public void createIndex(String name) throws XMLStreamException {
@@ -160,10 +183,63 @@ public class Client {
 	}
 
 	public void updateIndexProperties(String index, Properties properties) {
-		
+		// TODO
 	}
 
-	public void createDocument(String index, Document document) {
+	@SuppressWarnings("unchecked")
+	public void insertDocument(String index, Document document) throws XMLStreamException, HttpException, IOException {
+		PostMethod postMethod = new PostMethod(getIndexURI(index));
+		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		XMLStreamWriter xml = xmlOutputFactory.createXMLStreamWriter(buffer);
+		
+		xml.writeStartDocument();
+		xml.writeStartElement("entry");
+		xml.writeDefaultNamespace("http://www.w3.org/2005/Atom");
+		
+		xml.writeStartElement("content");
+		xml.writeAttribute("type", "xhtml");
+		xml.writeStartElement("div");
+		xml.writeDefaultNamespace("http://www.w3.org/1999/xhtml");
+		
+		List<Field> fields = document.getFields();
+		if (fields == null) {
+			xml.writeEmptyElement("dl");
+		} else {
+			xml.writeStartElement("dl");
+			for (Field field : fields) {
+				String name = field.name();
+				if (name == null) {
+					xml.writeEmptyElement("dt");
+				} else {
+					xml.writeStartElement("dt");
+					xml.writeCharacters(String.valueOf(name));
+					xml.writeEndElement();
+				}
+				
+				String value = field.stringValue();
+				if (value == null) {
+					xml.writeEmptyElement("dd");
+				} else {
+					xml.writeStartElement("dd");
+					xml.writeCharacters(String.valueOf(value));
+					xml.writeEndElement();
+				}
+			}
+			xml.writeEndElement(); // dl
+		}
+		
+		xml.writeEndElement(); // div
+		xml.writeEndElement(); // content
+		xml.writeEndElement(); // entry
+		xml.writeEndDocument();
+		
+		byte[] content = buffer.toByteArray();
+		postMethod.setRequestEntity(new ByteArrayRequestEntity(content));
+		
+		httpClient.executeMethod(postMethod);
+	}
+
+	public void updateDocument(String index, Document document) {
 		
 	}
 
---
+++ src/net/lucenews3/test/IndexPostTest.java
@@ -0,0 +1,9 @@
+package net.lucenews3.test;
+
+public class IndexPostTest {
+
+	public static void main(String... args) throws Exception {
+		
+	}
+
+}
